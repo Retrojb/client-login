ccb79f4fab5cc652ee57f9e3c8b13810ae433e0e
Retrojb <baltescartography@gmail.com>
2019-09-02 16:58:48 -0400

    added data service

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  devApiUrl = 'lmsDevDb/users';
+  headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json');
+  performance = { headers: this.headers };
+
+  constructor( private http: HttpClient ) { }
+
+  private handleError(error: any) {
+    console.error(error);
+    return throwError(error);
+  }
+
 }
