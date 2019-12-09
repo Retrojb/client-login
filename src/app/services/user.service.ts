9431d4a9018f0fb50b04b96fe6cbb8456bda2799
Retrojb <baltescartography@gmail.com>
2019-12-08 19:57:05 -0500

    refactor the user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,7 +3,6 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { tap, catchError, map} from 'rxjs/operators';
 import { User } from '../models/user.model';
-import { TouchSequence } from 'selenium-webdriver';
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +10,23 @@ import { TouchSequence } from 'selenium-webdriver';
 export class UserService {
 
   public API = 'api://5b2dd2e0-d425-4400-888c-d6a0286b516c/user-api';
-  public VENDEE_API = this.API + '/vendeeProfle';
+  public BASE_URL = 'http://localhost:4200'; //will change once served or create an environment
+  userProfile = 'mockUser';
+  userProfileUrl = `${this.BASE_URL + 'user-profile'}/${id}`;
 
   headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json'); // check against ktor headers
   performance = { headers: this.headers };
 
   constructor( private http: HttpClient) { }
 
-  private handleError(error: any) {  // swap out with Uncle Earl's Errors
+  private handleError(error: any) {
+  // swap out with Uncle Earl's Errors
     console.error(error);
     return throwError(error);
   }
 
   getAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.API)
+    return this.http.get<User[]>(this.BASE_URL)
       .pipe(
         tap(v => console.log(v),
         catchError(this.handleError)
@@ -32,27 +34,24 @@ export class UserService {
   }
 
   getUser(id: number) {
-    const url = `${this.API}/${id}`;
-    return this.http.get<User>(url).pipe(
+    return this.http.get<User>(this.userProfileUrl).pipe(
       tap(_ => console.log(`caught a wild morty id=${id}`)),
       catchError(this.handleError));
   }
 
-  addUser(user: User) {
-    this.http.post(`${this.API}/vendeeProfile`, user)
-    .subscribe(m => console.log('foo', m));
+  createUser(user: User) {
+    return this.http.post(this.userProfileUrl, user)
+      .subscribe(m => console.log('foo', m));
   }
 
   deleteUser(id): Observable<User> {
-    const url = `${this.API}/${id}`;
-    return this.http.delete<User>(url, this.performance)
-    .pipe( tap(_ => console.log(`dby id=${id}`)),
-    catchError(this.handleError));
+    return this.http.delete<User>(this.userProfileUrl, this.performance)
+      .pipe( tap(_ => console.log(`dby id=${id}`)),
+      catchError(this.handleError));
   }
 
-  updateUser(id, user): Observable<any> {
-    const url = `${this.API}/${id}`;
-    return this.http.put(url, user, this.performance).pipe(
+  updateUser(user): Observable<any> {
+    return this.http.put(this.userProfileUrl, user, this.performance).pipe(
       tap(_ => console.log(`updated user user=${user}`)),
       catchError(this.handleError)
     );
